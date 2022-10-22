ad7c8f6f193aabcd4547b4701038b9dceb27ab97
40-40 <110591549+40-40@users.noreply.github.com>
2022-10-22 14:29:44 -0500

    Lesson 61. Member functions
    what did I learn?  created new functions to help clean up code unlike js you need to specify the return type before the functions name and when in the cpp file you need to use the scope resolution operator as well as specify the actor like in the lines using "AMovingPlatform"

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -27,28 +27,32 @@ void AMovingPlatform::BeginPlay()
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+    MovePlatform(DeltaTime);
+    RotatePlatform(DeltaTime);
+}
 
-    // Move platform forwards
-        // Get current location
-
-        FVector CurrentLocation = GetActorLocation();
-        // Add vector to that location
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+    FVector CurrentLocation = GetActorLocation();
     CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
-        // Set the location
-        SetActorLocation(CurrentLocation);
-    // Send platform back if gone too far
-        // Check how far we've moved
-        float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-        // Reverse direction of motion if gone too far
-        if(DistanceMoved > MoveDistance) {
-            float OverShoot = DistanceMoved - MoveDistance;
-            FString Name = GetName();
-            UE_LOG(LogTemp, Display, TEXT("Actor: %s overshot by: %f"), *Name, OverShoot);
-            FVector MoveDirection = PlatformVelocity.GetSafeNormal();
-            
-            StartLocation = StartLocation + MoveDirection * MoveDistance;
-            SetActorLocation(StartLocation);
+    SetActorLocation(CurrentLocation);
 
-            PlatformVelocity = -PlatformVelocity;
-        }
+    float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+
+    if(DistanceMoved > MoveDistance) 
+    {
+        float OverShoot = DistanceMoved - MoveDistance;
+        FString Name = GetName();
+        UE_LOG(LogTemp, Display, TEXT("Actor: %s overshot by: %f"), *Name, OverShoot);
+        
+        FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+        StartLocation = StartLocation + MoveDirection * MoveDistance;
+        SetActorLocation(StartLocation);
+        PlatformVelocity = -PlatformVelocity;
+    }
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+    UE_LOG(LogTemp, Display, TEXT("%s rotating"), *GetName());
 }
