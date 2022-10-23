46f0c70ea272bfd9c4cf2de39bd0201e57711208
40-40 <110591549+40-40@users.noreply.github.com>
2022-10-22 21:26:00 -0500

    Lesson 62. Return statements
    what did I learn? just created a new function but made it return a bool, also refactored code to be cleaner

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -33,26 +33,32 @@ void AMovingPlatform::Tick(float DeltaTime)
 
 void AMovingPlatform::MovePlatform(float DeltaTime)
 {
-    FVector CurrentLocation = GetActorLocation();
-    CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
-    SetActorLocation(CurrentLocation);
-
-    float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-
-    if(DistanceMoved > MoveDistance) 
-    {
-        float OverShoot = DistanceMoved - MoveDistance;
-        FString Name = GetName();
-        UE_LOG(LogTemp, Display, TEXT("Actor: %s overshot by: %f"), *Name, OverShoot);
-        
+    if(ShouldPlatformReturn()) 
+    {        
         FVector MoveDirection = PlatformVelocity.GetSafeNormal();
         StartLocation = StartLocation + MoveDirection * MoveDistance;
         SetActorLocation(StartLocation);
         PlatformVelocity = -PlatformVelocity;
+    }
+    else
+    {
+        FVector CurrentLocation = GetActorLocation();
+        CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
+        SetActorLocation(CurrentLocation);
     }
 }
 
 void AMovingPlatform::RotatePlatform(float DeltaTime)
 {
     UE_LOG(LogTemp, Display, TEXT("%s rotating"), *GetName());
+}
+
+bool AMovingPlatform::ShouldPlatformReturn()
+{
+    return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved()
+{
+    return FVector::Dist(StartLocation, GetActorLocation());
 }
